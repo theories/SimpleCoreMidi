813ab409b54a21aeecd0a8f7200c398c54e23872
Thierry Sansaricq <thierry@Thierrys-MBP.lan>
2015-08-18 09:55:02 -0400

    Initial Commit

---
+++ SimpleCoreMidi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleCoreMidi
+//
+//  Created by Thierry Sansaricq on 8/18/15.
+//  Copyright (c) 2015 Thierry Sansaricq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
