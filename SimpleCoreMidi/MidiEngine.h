521a38f92ec9868d07bb310a9e7f451324c48e67
Thierry Sansaricq <thierry@Thierrys-MBP.lan>
2015-08-18 10:06:02 -0400

    Initial Project Set up

---
+++ SimpleCoreMidi/MidiEngine.h
@@ -0,0 +1,13 @@
+//
+//  MidiEngine.h
+//  SimpleCoreMidi
+//
+//  Created by Thierry Sansaricq on 8/18/15.
+//  Copyright (c) 2015 Thierry Sansaricq. All rights reserved.
+//
+
+#ifndef SimpleCoreMidi_MidiEngine_h
+#define SimpleCoreMidi_MidiEngine_h
+
+
+#endif
---
+++ SimpleCoreMidi/MidiEngine.m
@@ -0,0 +1,10 @@
+//
+//  MidiEngine.m
+//  SimpleCoreMidi
+//
+//  Created by Thierry Sansaricq on 8/18/15.
+//  Copyright (c) 2015 Thierry Sansaricq. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AudioToolbox/MusicPlayer.h>
